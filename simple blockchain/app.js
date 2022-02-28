c8a9d861b9e6da376224236c7bb334b6d17790f8
Emre Pbu <33737009+EmrePbu@users.noreply.github.com>
2022-02-28 21:32:51 +0300

    Add project files.

---
+++ simple blockchain/app.js
@@ -0,0 +1,44 @@
+const SHA256 = require("crypto-js/sha256");
+
+class Block {
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash()
+    }
+
+    calculateHash() {
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
+}
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock() {
+        return new Block(0, "01/01/1970", "Genesis Block", "0");
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+}
+
+let firat_chain = new Blockchain();
+firat_chain.addBlock(new Block(1, "01/01/2017", { amount: 4 }));
+firat_chain.addBlock(new Block(2, "01/01/2018", { amount: 10 }));
+
+console.log(JSON.stringify(firat_chain, null, 4));
+
+
+console.log("finish");
