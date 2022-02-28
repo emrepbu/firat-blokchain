0a33485fa4ff57ba3fb17c5a4b191354bedb5e8f
Emre Pbu <33737009+EmrePbu@users.noreply.github.com>
2022-02-28 22:17:10 +0300

    Change IDE

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# simple-blockchain
+# simple blockchain
+
+
--- app.js
+++ app.js
@@ -32,13 +32,41 @@ class Blockchain {
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain; ++i) {
+
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
 
 let firat_chain = new Blockchain();
 firat_chain.addBlock(new Block(1, "01/01/2017", { amount: 4 }));
-firat_chain.addBlock(new Block(2, "01/01/2018", { amount: 10 }));
-
-console.log(JSON.stringify(firat_chain, null, 4));
+firat_chain.addBlock(new Block(2, "01/01/2018", { amount: 1 }));
 
 
-console.log("finish");
+firat_chain.chain[1].data = { amount: 999 };
+console.log("Is blockchain valid?: " + firat_chain.isChainValid());
+
+
+firat_chain.chain[1].hash = firat_chain.chain[1].calculateHash();
+
+
+console.log("Is blockchain valid?: " + firat_chain.isChainValid());
+// console.log(JSON.stringify(firat_chain, null, 4));
+
+
+
+
