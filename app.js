0efdbcddf9f375f76da5990d074608a0ddf28311
Emre Pbu <33737009+EmrePbu@users.noreply.github.com>
2022-03-14 19:20:57 +0300

    Add nonce value

--- app.js
+++ app.js
@@ -6,17 +6,28 @@ class Block {
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
-        this.hash = this.calculateHash()
+        this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty) {
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log("Block mined: " + this.hash);
     }
 }
 
 class Blockchain {
     constructor() {
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 2;
     }
 
     createGenesisBlock() {
@@ -30,6 +41,7 @@ class Blockchain {
     addBlock(newBlock) {
         newBlock.previousHash = this.getLatestBlock().hash;
         newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
@@ -52,8 +64,15 @@ class Blockchain {
 }
 
 let firat_chain = new Blockchain();
+
+console.log("Mining block 1...");
 firat_chain.addBlock(new Block(1, "01/01/2017", {amount: 4}));
+console.log("Mining block 2...");
 firat_chain.addBlock(new Block(2, "01/01/2018", {amount: 1}));
+console.log("Mining block 3...");
+firat_chain.addBlock(new Block(3, "01/01/2019", {amount: 2}));
+console.log("Mining block 4...");
+firat_chain.addBlock(new Block(4, "01/01/2020", {amount: 3}));
 
 console.log(JSON.stringify(firat_chain, null, 4));
 
