aef192e3e0932e3945b6fb4d6f231db4b4072448
Emre Pbu <33737009+EmrePbu@users.noreply.github.com>
2022-03-23 20:52:22 +0300

    Mining rewards & transactions

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 const SHA256 = require("crypto-js/sha256");
 
+class Transactions {
+    constructor(fromAddress, toAddress, amount) {
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
+    }
+}
+
+
 class Block {
-    constructor(index, timestamp, data, previousHash = '') {
-        this.index = index;
+    constructor(timestamp, transactions, previousHash = '') {
         this.timestamp = timestamp;
-        this.data = data;
+        this.transactions = transactions;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
         this.nonce = 0;
     }
 
     calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+        return SHA256(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).toString();
     }
 
     mineBlock(difficulty) {
@@ -20,29 +28,56 @@ class Block {
             this.hash = this.calculateHash();
         }
 
-        console.log("Block mined: " + this.hash);
+        console.log("BLOCK MINED: " + this.hash + " WITH NONCE: " + this.nonce);
     }
 }
 
 class Blockchain {
     constructor() {
         this.chain = [this.createGenesisBlock()];
-        this.difficulty = 2;
+        this.difficulty = 3;
+        this.pendTransactions = [];
+        this.miningReward = 100;
     }
 
     createGenesisBlock() {
-        return new Block(0, "01/01/1970", "Genesis Block", "0");
+        return new Block("01/01/1970", "Genesis Block", "0");
     }
 
     getLatestBlock() {
         return this.chain[this.chain.length - 1];
     }
 
-    addBlock(newBlock) {
-        newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
-        newBlock.mineBlock(this.difficulty);
-        this.chain.push(newBlock);
+    minePendingTransactions(miningRewardAddress) {
+        let block = new Block(Date.now(), this.pendTransactions);
+        block.mineBlock(this.difficulty);
+
+        console.log("BLOCK SUCCESSFULLY MINED!");
+        this.chain.push(block);
+
+        this.pendTransactions = [
+            new Transactions(null, miningRewardAddress, this.miningReward)
+        ];
+    }
+
+    createTransaction(transaction) {
+        this.pendTransactions.push(transaction);
+    }
+
+    getBalanceOfAddress(address) {
+        let balance = 0;
+
+        for (const block of this.chain) {
+            for (const trans of block.transactions) {
+                if (trans.fromAddress === address) {
+                    balance -= trans.amount;
+                }
+                if (trans.toAddress === address) {
+                    balance += trans.amount;
+                }
+            }
+        }
+        return balance;
     }
 
     isChainValid() {
@@ -64,16 +99,28 @@ class Blockchain {
 }
 
 let firat_chain = new Blockchain();
+firat_chain.createTransaction(new Transactions("address1", "address2", 100));
+firat_chain.createTransaction(new Transactions("address2", "address1", 10));
 
-console.log("Mining block 1...");
-firat_chain.addBlock(new Block(1, "01/01/2017", {amount: 4}));
-console.log("Mining block 2...");
-firat_chain.addBlock(new Block(2, "01/01/2018", {amount: 1}));
-console.log("Mining block 3...");
-firat_chain.addBlock(new Block(3, "01/01/2019", {amount: 2}));
-console.log("Mining block 4...");
-firat_chain.addBlock(new Block(4, "01/01/2020", {amount: 3}));
+console.log("\nStarting the miner...");
+firat_chain.minePendingTransactions("firat_address");
 
-console.log(JSON.stringify(firat_chain, null, 4));
+console.log("\nBalance of firat_address: " + firat_chain.getBalanceOfAddress("firat_address"));
+
+
+console.log("\nStarting the miner again...");
+firat_chain.minePendingTransactions("firat_address");
+
+console.log("\nBalance of firat_address: " + firat_chain.getBalanceOfAddress("firat_address"));
+
+
+// console.log("Mining block 1...");
+// firat_chain.addBlock(new Block("01/01/2017", {amount: 4}));
+// console.log("Mining block 2...");
+// firat_chain.addBlock(new Block("01/01/2018", {amount: 1}));
+
+// console.log(JSON.stringify(firat_chain, null, 4));
+
+
 
 
